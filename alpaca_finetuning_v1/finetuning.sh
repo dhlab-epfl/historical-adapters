20581a63b72d2287128df19899431574f9d22095
sori424 <sori424@gmail.com>
2023-05-06 09:44:19 +0000

    path changes

--- alpaca_finetuning_v1/finetuning.sh
+++ alpaca_finetuning_v1/finetuning.sh
@@ -1,7 +1,9 @@
-torchrun --nproc_per_node 8 finetuning.py \
+TARGET_FOLDER='/nlpdata1/home/sooh/llama'
+
+torchrun --nproc_per_node 4 finetuning.py \
     --model Llama7B_adapter \
     --llama_model_path $TARGET_FOLDER/ \
-    --data_path $DATA_PATH/alpaca_data.json \
+    --data_path alpaca_data.json \
     --adapter_layer 30 \
     --adapter_len 10 \
     --max_seq_len 512 \
@@ -10,4 +12,4 @@ torchrun --nproc_per_node 8 finetuning.py \
     --warmup_epochs 2 \
     --blr 9e-3 \
     --weight_decay 0.02 \
-    --output_dir ./checkpoint/
+    --output_dir /nlpdata1/home/sooh/llama_adapter/checkpoint/
