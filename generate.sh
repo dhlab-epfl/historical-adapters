7516bfd453dce219c32c201020877eae41272ba0
sori424 <sori424@gmail.com>
2023-05-25 23:48:22 +0000

    result

--- generate.sh
+++ generate.sh
@@ -1,4 +1,4 @@
 torchrun --nproc_per_node 1 example-multiple.py \
 --ckpt_dir /data1/data/sooh-data/llama/7B/ \
 --tokenizer_path /data1/data/sooh-data/llama/tokenizer.model \
---adapter_path /data1/data/sooh-data/llama/checkpoint2/adapter_adapter_len10_layer30_epoch5.pth
+--adapter_path /data1/data/sooh-data/llama/checkpoint3/adapter_adapter_len10_layer30_epoch8.pth
